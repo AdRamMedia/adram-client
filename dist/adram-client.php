7b65f517af8a7fad009f983da10065de66dc39a4
Dmitry Bergstein <bergstein@trickyplan.com>
2017-07-31 19:26:53 +0300

    Initial Commit.

---
+++ dist/adram-client.php
@@ -0,0 +1,40 @@
+<?php
+    
+    define ('ADRAM_ROOT', dir(realpath (__DIR__))->path);
+    
+    function adram_get_code()
+    {
+        $Options = json_decode(file_get_contents(ADRAM_ROOT.'/adram-client.json'), true);
+        $Options['AdRam']['React']['Options']['AdRecovery']['Code'] = file_get_contents(ADRAM_ROOT.'/unprotected-code.html');
+        
+        $CURL = curl_init();
+        curl_setopt_array($CURL,
+            [
+                CURLOPT_URL => 'https://adram.local/api/2.2/raw/AdRam/GetDetectAndReactCode',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 5,
+                CURLOPT_TIMEOUT => 5,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => json_encode($Options),
+                CURLOPT_HTTPHEADER =>
+                [
+                    'cache-control: no-cache',
+                    'content-type: application/json'
+                ]
+            ]);
+        $Response = curl_exec($CURL);
+        $Error = curl_error($CURL);
+        curl_close($CURL);
+        
+        if ($Error)
+            return $Error;
+        else
+        {
+            file_put_contents(ADRAM_ROOT.'/protected-code.html' , $Response);
+            return true;
+        }
+    }
+    
+    adram_get_code();
